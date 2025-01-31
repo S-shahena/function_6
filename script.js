ea65cfd87cc7485f3977800b2d4b4c9048b41b96
Mst. Shahena Akter <sa4389310@gmail.com>
2025-01-31 16:27:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function sum(num1, num2){
+console.log(num1 + num2);
+}
+
+sum(15, 25);
+
+function multi(num1, num2){
+console.log(num1 - num2);
+}
+
+multi(55, 30);
+
+function myName(num1, num2){
+console.log(num1 * num2);
+}
+
+myName(130, 46);
+
+function de(num1, num2){
+console.log(num1 / num2);
+}
+
+de(130, 46);
